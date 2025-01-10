719fd16afc93069034009328e8cbbb9a5d302ddd
Joubert Diego <jdrsjdrsoares@gmail.com>
2025-01-10 19:16:10 +0100

    brand to unique=True

--- products/models.py
+++ products/models.py
@@ -44,7 +44,7 @@ class PackageUnit(models.Model):
         return f"{self.name} ({self.symbol})"
 
 class Brand(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     image = models.ImageField(upload_to='brands/images/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
