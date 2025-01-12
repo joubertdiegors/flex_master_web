f37ee5a6c524505a5947d195c7db4a02cc1af6f2
Joubert Diego <jdrsjdrsoares@gmail.com>
2025-01-12 23:22:14 +0100

    remove all google analytics api

--- app/settings.py
+++ app/settings.py
@@ -5,12 +5,6 @@ from decouple import config, Csv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Google Analytics
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-GOOGLE_APPLICATION_CREDENTIALS = os.path.join(BASE_DIR, "GoogleAnalytics.json")
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = GOOGLE_APPLICATION_CREDENTIALS
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
