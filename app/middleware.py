e5af7b4734ee6ed5c17f9e24cef06a20125c38ee
Joubert Diego <jdrsjdrsoares@gmail.com>
2025-01-28 20:56:40 +0100

    adjust layout cart

--- app/middleware.py
+++ app/middleware.py
@@ -9,7 +9,7 @@ class AdminPermissionMiddleware:
         return response
 
     def process_view(self, request, view_func, view_args, view_kwargs):
-        if request.path.startswith('/administration/') or request.path.startswith('/dashboard/') or request.path.startswith('/admin/'):
+        if request.path.startswith('/administration/') or request.path.startswith('/dashboard/') or request.path.startswith('/adm/'):
             if not request.user.is_authenticated or (not request.user.is_superuser and not request.user.is_staff):
                 return redirect('/')
         return None
