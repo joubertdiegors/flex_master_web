74d1c191ef9cabdb85fbc0c5e3208b355bab2879
Joubert Diego <jdrsjdrsoares@gmail.com>
2025-05-04 23:32:31 +0200

    adjust user end point

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import UserProfile
 
-# Register your models here.
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = "Perfil do Usuário"
+
+# Estende o admin padrão do User
+class UserAdmin(admin.ModelAdmin):
+    inlines = [UserProfileInline]
+    list_display = ("username", "email", "first_name", "last_name", "is_staff")
+
+# Remove o admin padrão e registra o novo
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    access_code = models.CharField(max_length=10, unique=True, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.access_code or 'sem código'}"
